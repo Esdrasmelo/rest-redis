47d65e42bbfdc42517761e23d298bd1a0c20c430
Esdrasmelo <emelomotta@gmail.com>
2022-07-29 12:04:53 -0300

    adding new migration

--- prisma/migrations/20220729142642_create_initial_tables/migration.sql
+++ prisma/migrations/20220729142642_create_initial_tables/migration.sql
@@ -23,3 +23,16 @@ CREATE TABLE "jobs" (
 
     CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "color" VARCHAR NOT NULL,
+    "price" VARCHAR NOT NULL,
+    "description" VARCHAR NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
